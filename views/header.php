8f40787ebafe43055d6c9b5230dc6b7a5873024a
scroutch <cecile.duhain@gmail.com>
2023-05-23 20:10:00 +0200

    modification du lien

--- views/header.php
+++ views/header.php
@@ -4,7 +4,7 @@
         <li>About me</li>
         <li>tarifs</li>
         <li>contacts</li>
-        <li><a href="../views/connexion.php">Connexion</a></li>
+        <li><a href="index.php?page=3">Connexion</a></li>
     </ul>
     <div id="presentation">
         <h2>Wedding photographer</h2>
