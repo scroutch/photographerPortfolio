7a5a220b149feaf7dfe67a656685f026da3a32b3
scroutch <cecile.duhain@gmail.com>
2023-06-01 17:30:59 +0200

    modification de la connexion à la bdd

--- models/bdd.php
+++ models/bdd.php
@@ -5,7 +5,7 @@ if (!isset($_SESSION)) {
 }
 
 try {
-    $bdd = new PDO('mysql:host=127.0.0.1:8889;dbname=photographe;charset=utf8', 'root', 'root');
+    $bdd = new PDO('mysql:host=127.0.0.1:3306;dbname=photographe;charset=utf8', 'root', '');
     // $bdd = new PDO('mysql:mysql.hostinger.com;dbname=u548150219_casanied;charset=utf8', 'u548150219_admin', 'Jv7ww6j9!');
     $bdd->setATTRIBUTE(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $bdd->setATTRIBUTE(PDO::ERRMODE_SILENT, PDO::ERRMODE_WARNING);
